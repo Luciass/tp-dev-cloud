e3fae6ecaa5eb055ebbe9b21b530fc7ad136c3ae
Luciass <noha.flahaut@gmail.com>
2025-04-14 12:30:32 +0200

    change

---
+++ backend.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, request, jsonify, render_template
+import requests
+import os
+
+app = Flask(__name__)
+
+FIREBASE_DB_URL = "https://tp-cloud-14-04-25-default-rtdb.europe-west1.firebasedatabase.app/messages.json"
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/envoyer', methods=['POST'])
+def envoyer():
+    data = request.get_json()
+    response = requests.post(FIREBASE_DB_URL, json=data)
+    if response.ok:
+        return jsonify({"status": "envoyé"}), 200
+    return jsonify({"status": "échec", "detail": response.text}), 500
+
+@app.route('/messages', methods=['GET'])
+def messages():
+    response = requests.get(FIREBASE_DB_URL)
+    if response.ok:
+        return jsonify(response.json()), 200
+    return jsonify({"error": "Erreur de lecture"}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
